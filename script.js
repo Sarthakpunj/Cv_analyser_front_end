6ae3e97ecf9d549fd7cfc60095956fadd41b4831
Sarthakpunj <121757517+Sarthakpunj@users.noreply.github.com>
2024-09-08 00:41:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+function openUploadModal() {
+    document.getElementById('upload-modal').style.display = 'block';
+}
+
+function closeUploadModal() {
+    document.getElementById('upload-modal').style.display = 'none';
+}
